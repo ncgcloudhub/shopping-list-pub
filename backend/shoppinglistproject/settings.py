f263bb4d6be948c08263cc7792adbf7206ed14c9
AutomateGuru <saiful@newcomputergen.com>
2023-10-27 02:07:46 -0400

    adding frontend react app

--- backend/shoppinglistproject/settings.py
+++ backend/shoppinglistproject/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
-    'lists',
+    'backend.lists',
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",

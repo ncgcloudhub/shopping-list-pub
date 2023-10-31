1fa1282f17d3c110e657b835e34d5ce0c360fae6
AutomateGuru <saiful@newcomputergen.com>
2023-10-31 00:06:58 -0400

    update shoppinglist.js

--- frontend/shopping-list-app/src/components/ShoppingList.js
+++ frontend/shopping-list-app/src/components/ShoppingList.js
@@ -5,8 +5,20 @@ import {
   DialogActions, DialogContent, DialogTitle, Container, FormControl, InputLabel, Select, MenuItem
 } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Autocomplete from '@mui/material/Autocomplete';
 
 
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
 
 
 function ShoppingList() {
@@ -16,6 +28,8 @@ function ShoppingList() {
     const [currentItem, setCurrentItem] = useState(null);  // The item currently being updated
     const [updateDialogOpen, setUpdateDialogOpen] = useState(false);
     const [categories, setCategories] = useState([]);
+    const [newItemCategory, setNewItemCategory] = useState('');
+
 
 
 
@@ -24,6 +38,7 @@ function ShoppingList() {
             .then(response => response.json())
             .then(data => setItems(data));
     }, []);
+
 
     const handleAdd = () => {
         fetch('http://localhost:8000/lists/api/items/', {
@@ -37,7 +52,8 @@ function ShoppingList() {
         .then(data => {
             setItems([...items, data]);
             setOpenDialog(false);
-        });
+        })
+        .catch(error => console.error(error));
     };
 
     const handleUpdate = (itemId) => {
@@ -69,25 +85,10 @@ function ShoppingList() {
           method: 'DELETE',
       })
       .then(() => {
-          setItems(prevItems => {
-              return prevItems.filter(item => item.id !== itemId);
-          });
+          setItems(prevItems => prevItems.filter(item => item.id !== itemId));
       });
     };
   
-
-    const theme = createTheme({
-      palette: {
-        mode: 'dark',
-        primary: {
-          main: '#1976d2',
-        },
-        secondary: {
-          main: '#dc004e',
-        },
-      },
-    });
-    
     return (
       <ThemeProvider theme={theme}>
           <div>
@@ -135,43 +136,60 @@ function ShoppingList() {
                   </TableContainer>
                   {/* Dialog for adding items */}
                   <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
-                      <DialogTitle>Add Item</DialogTitle>
-                      <DialogContent>
-                          <TextField 
-                              fullWidth
-                              label="Item Name" 
-                              value={newItemName}
-                              onChange={(e) => setNewItemName(e.target.value)}
-                          />
-                      </DialogContent>
-                      <DialogActions>
-                          <Button variant="contained" color="primary" onClick={handleAdd}>
-                              Add
-                          </Button>
-                          <Button variant="contained" onClick={() => setOpenDialog(false)}>
-                              Cancel
-                          </Button>
-                      </DialogActions>
-                  </Dialog>
-                  <Dialog open={updateDialogOpen} onClose={() => setUpdateDialogOpen(false)}>
-                    <DialogTitle>Update Item</DialogTitle>
+                    <DialogTitle>Add Item</DialogTitle>
                     <DialogContent>
                         <TextField 
+                            fullWidth
                             label="Item Name" 
-                            value={currentItem?.name || ''}
-                            onChange={(e) => setCurrentItem(prev => ({...prev, name: e.target.value}))}
+                            value={newItemName}
+                            onChange={(e) => setNewItemName(e.target.value)}
                         />
-                        <TextField 
-                            label="Category"
-                            value={currentItem?.category.name || ''}
-                            onChange={(e) => setCurrentItem(prev => ({...prev, category: {name: e.target.value}}))}
-                        />
+                        {/* Added a dropdown to select category for the new item */}
+                        <Select
+                            fullWidth
+                            value={newItemCategory}
+                            onChange={(e) => setNewItemCategory(e.target.value)}
+                        >
+                            {/* For now, hard-coded categories */}
+                            <MenuItem value="Fruits">Fruits</MenuItem>
+                            <MenuItem value="Vegetables">Vegetables</MenuItem>
+                            <MenuItem value="Meat">Meat</MenuItem>
+                            {/* Add other categories as needed */}
+                        </Select>
                     </DialogContent>
                     <DialogActions>
-                        <Button variant="contained" color="primary" onClick={submitUpdate}>Confirm</Button>
-                        <Button variant="contained" color="default" onClick={() => setUpdateDialogOpen(false)}>Cancel</Button>
+                        <Button variant="contained" color="primary" onClick={handleAdd}>
+                            Add
+                        </Button>
+                        <Button variant="contained" onClick={() => setOpenDialog(false)}>
+                            Cancel
+                        </Button>
                     </DialogActions>
                 </Dialog>
+                    <Dialog open={updateDialogOpen} onClose={() => setUpdateDialogOpen(false)}>
+                        <DialogTitle>Update Item</DialogTitle>
+                        <DialogContent>
+                            <TextField 
+                                label="Item Name" 
+                                value={currentItem?.name || ''}
+                                onChange={(e) => setCurrentItem(prev => ({...prev, name: e.target.value}))}
+                            />
+                            <Autocomplete
+                                value={currentItem?.category}
+                                onChange={(event, newValue) => {
+                                    setCurrentItem(prev => ({ ...prev, category: newValue }));
+                                }}
+                                options={categories}  // This should be fetched from your backend. For now, we assume a hardcoded list.
+                                getOptionLabel={(option) => option.name}
+                                renderInput={(params) => <TextField {...params} label="Category" />}
+                            />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button variant="contained" color="primary" onClick={submitUpdate}>Confirm</Button>
+                            <Button variant="contained" color="default" onClick={() => setUpdateDialogOpen(false)}>Cancel</Button>
+                        </DialogActions>
+                    </Dialog>
+
               </Container>
           </div>
         </ThemeProvider>
